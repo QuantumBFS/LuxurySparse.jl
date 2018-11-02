52393f7dd86ec4b4d15b3aab2eae3366b8557ad5
Roger-luo <hiroger@qq.com>
2018-11-01 23:33:28 -0400

    bug fix

--- src/arraymath.jl
+++ src/arraymath.jl
@@ -70,5 +70,5 @@ for op in [:+, :-, :(==), :≈]
     end
 
 end
-+(d1::IMatrix{Na, Ta}, d2::IMatrix{Nb, Tb}) where {Na, Nb, Ta, Tb} = d1==d2 ? Diagonal(fill(promote_types(Ta, Tb)(2), Na)) : throw(DimensionMismatch())
--(d1::IMatrix{Na, Ta}, d2::IMatrix{Nb, Tb}) where {Na, Ta, Nb, Tb} = d1==d2 ? spzeros(promote_types(Ta, Tb), Na, Na) : throw(DimensionMismatch())
++(d1::IMatrix{Na, Ta}, d2::IMatrix{Nb, Tb}) where {Na, Nb, Ta, Tb} = d1==d2 ? Diagonal(fill(promote_type(Ta, Tb)(2), Na)) : throw(DimensionMismatch())
+-(d1::IMatrix{Na, Ta}, d2::IMatrix{Nb, Tb}) where {Na, Ta, Nb, Tb} = d1==d2 ? spzeros(promote_type(Ta, Tb), Na, Na) : throw(DimensionMismatch())
