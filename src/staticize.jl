d99c036d4ac46375144439aa646f29440cc3cda3
Roger-luo <hiroger@qq.com>
2019-05-22 01:27:23 -0400

    fix empty matrix bug

--- src/staticize.jl
+++ src/staticize.jl
@@ -28,6 +28,7 @@ staticize(A::AbstractVector) = SVector{length(A)}(A)
 staticize(A::Diagonal) = SDiagonal{size(A,1)}((A.diag...,))
 staticize(A::PermMatrix) = PermMatrix(SVector{size(A,1)}(A.perm), SVector{size(A, 1)}(A.vals))
 function staticize(A::SparseMatrixCSC)
+    isempty(A) && return SSparseMatrixCSC(A.m, A.n, SVector{0, eltype(A)}(), SVector{0, eltype(A)}(), SVector{0, eltype(A)}())
     SSparseMatrixCSC(A.m, A.n, SVector{length(A.colptr)}(A.colptr), SVector{length(A.rowval)}(A.rowval), SVector{length(A.nzval)}(A.nzval))
 end
 
