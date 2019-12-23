694916ff323cc1e9f7f8a6609c58f89526adb4a8
autodocs <autodocs>
2019-12-23 04:28:22 +0000

    build based on 4e50b5b

--- latest/search_index.js
+++ latest/search_index.js
@@ -89,14 +89,6 @@ var documenterSearchIndex = {"docs": [
 },
 
 {
-    "location": "luxurysparse/#LuxurySparse.SSparseMatrixCSC",
-    "page": "Manual",
-    "title": "LuxurySparse.SSparseMatrixCSC",
-    "category": "type",
-    "text": "SSparseMatrixCSC{Tv,Ti<:Integer, NNZ, NP} <: AbstractSparseMatrix{Tv,Ti}\n\nstatic version of SparseMatrixCSC\n\n\n\n\n\n"
-},
-
-{
     "location": "luxurysparse/#LuxurySparse.SparseMatrixCOO",
     "page": "Manual",
     "title": "LuxurySparse.SparseMatrixCOO",
