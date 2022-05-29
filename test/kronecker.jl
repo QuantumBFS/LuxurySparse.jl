1662e810703edbd716e4a084378997aedea78532
GiggleLiu <cacate0129@gmail.com>
2022-05-29 01:51:29 -0400

    fix nightly test

--- test/kronecker.jl
+++ test/kronecker.jl
@@ -8,14 +8,12 @@ import LuxurySparse: IMatrix, PermMatrix
     sp = sprand(ComplexF64, 4, 4, 0.5)
     ds = rand(ComplexF64, 4, 4)
     pm = PermMatrix([2, 3, 4, 1], randn(4))
-    @show typeof(pm)
-    @show @which PermMatrix([2, 3, 4, 1], randn(4))
     pm = PermMatrix([2, 3, 4, 1], randn(4))
     v = [0.5, 0.3im, 0.2, 1.0]
     dv = Diagonal(v)
 
-    for source in [p1, sp, ds, dv, pm],
-            target in [p1, sp, ds, dv, pm]
+    for source in Any[p1, sp, ds, dv, pm],
+            target in Any[p1, sp, ds, dv, pm]
         lres = kron(source, target)
         rres = kron(target, source)
         flres = kron(Matrix(source), Matrix(target))
