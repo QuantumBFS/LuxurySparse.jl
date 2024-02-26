07a534be7a7cdc3bc023625292184ad961d3f680
GiggleLiu <cacate0129@gmail.com>
2024-02-26 11:15:01 +0800

    improve test coverage

--- src/LuxurySparse.jl
+++ src/LuxurySparse.jl
@@ -6,6 +6,7 @@ using SparseArrays: SparseMatrixCSC
 using SparseArrays.HigherOrderFns
 using Base: @propagate_inbounds
 using LinearAlgebra
+import SparseArrays: findnz, nnz
 using LinearAlgebra: StructuredMatrixStyle
 using Base.Broadcast:
     BroadcastStyle, AbstractArrayStyle, Broadcasted, DefaultArrayStyle, materialize!
